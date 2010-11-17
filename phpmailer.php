eb1bfe616e3d00d69044a9f8d45cc42b01ffd42d
Seth Wilson <sethdubya@gmail.com>
2010-11-17 00:44:26 -0500

    Adding event type dropdown slector in the contact us lightbox

--- phpmailer.php
+++ phpmailer.php
@@ -8,6 +8,8 @@ if (!empty($_POST)) {
 	$message = "Name: ".$_POST['name']."\n";
 	$message .= "Email: ".$_POST['email']."\n";
 	$message .= "Daytime Phone: ".$_POST['dayphone']."\n";
+	$message .= "Event Date: ".$_POST['event_date']."\n";
+	$message .= "Event Type: ".$_POST['eventtype']."\n";
 	$message .= "Who Referred You: ".$_POST['referredfrom']."\n";
 	$message .= "If Other: ".$_POST['other']."\n";
 	
